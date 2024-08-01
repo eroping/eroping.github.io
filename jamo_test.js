99f1787435ef69eb1af37e571439bfdb7f6d9e05
eroping <109841164+eroping@users.noreply.github.com>
2024-08-01 19:20:11 +0900

    자모 분리결합+영한 키값 변환 구현

--- jamo_test.js
+++ jamo_test.js
@@ -9,14 +9,16 @@ const concateBtn = document.querySelector("#concat");
 const jamoToEngBtn = document.querySelector("#jamo_eng");
 const engToJamoBtn = document.querySelector("#eng_jamo");
 
+const p = document.querySelector("p");
 
 //자모 분리 테스트
 separateBtn.addEventListener("click",function(){
     const strValue = new String(inputData.value);
     let jamoOper=new Jamo(strValue);
     let result=jamoOper.getSeparateString();
-    console.log(result); 
+    p.innerText += "자모 분리 : "+result+"\n";
     inputData.value="";
+    p.scrollTop = p.scrollHeight;
 });
 
 //자모 결합 테스트
@@ -24,8 +26,9 @@ concateBtn.addEventListener("click",function(){
     const strValue = new String(inputData.value);
     let jamoOper=new Jamo(strValue);
     let result=jamoOper.getBindString();
-    console.log(result); 
+    p.innerText += "자모 결합 : "+result+"\n";
     inputData.value="";
+    p.scrollTop = p.scrollHeight;
 });
 
 //한글 => 영문자 변환
@@ -33,8 +36,9 @@ jamoToEngBtn.addEventListener("click",function(){
     const strValue = new String(inputData.value);
     let kor = new EncodingKorEng(strValue);
     let result = kor.encodeToEng();
-    console.log(result);
+    p.innerText += "한글=>영문 : "+result+"\n";
     inputData.value="";
+    p.scrollTop = p.scrollHeight;
 });
 
 //영문자 => 한글 변환
@@ -42,6 +46,7 @@ engToJamoBtn.addEventListener("click",function(){
     const strValue = new String(inputData.value);
     let eng = new EncodingKorEng(strValue);
     let result = eng.encodeToKor(); 
-    console.log(result);
+    p.innerText += "영문=>한글 : "+result+"\n";
     inputData.value="";
+    p.scrollTop = p.scrollHeight;
 });
